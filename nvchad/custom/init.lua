17c3a69ec6e4a4ae6e8071ce25e1ac1aa0f5c25e
Tom Speak <tom@bere.al>
2023-08-03 15:18:25 +0100

    fix go lspsaga

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -15,7 +15,7 @@ opt.termguicolors = true
 opt.updatetime = 250
 opt.timeout = true
 opt.timeoutlen = 300
-opt.wildignore:append "*/node_modules/*"
+opt.wildignore:append("*/node_modules/*")
 opt.inccommand = "nosplit"
 opt.ignorecase = true
 opt.incsearch = true
@@ -29,3 +29,4 @@ opt.backspace = "indent,eol,start" -- make backspace work as expected
 opt.swapfile = false -- disable the .swp files vim creates
 opt.splitright = true -- open horizontal splits to the right
 opt.splitbelow = true -- open vertical splits below
+opt.signcolumn = "yes"
--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,217 +1,225 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = overrides.cmp,
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = overrides.cmp,
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = overrides.telescope,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = overrides.telescope,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "tpope/vim-surround",
-    lazy = false,
-  },
-  {
-    "tpope/vim-repeat",
-    lazy = false,
-  },
-  {
-    "tpope/vim-sleuth",
-    lazy = false,
-  },
+	{
+		"tpope/vim-surround",
+		lazy = false,
+	},
+	{
+		"tpope/vim-repeat",
+		lazy = false,
+	},
+	{
+		"tpope/vim-sleuth",
+		lazy = false,
+	},
 
-  -- autoclose tags in html, jsx only
-  {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
+	-- autoclose tags in html, jsx only
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    config = function()
-      require "custom.configs.zenmode"
-    end,
-  },
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		config = function()
+			require("custom.configs.zenmode")
+		end,
+	},
 
-  {
-    "f-person/auto-dark-mode.nvim",
-    event = "VeryLazy",
+	{
+		"f-person/auto-dark-mode.nvim",
+		event = "VeryLazy",
 
-    config = function()
-      local auto_dark_mode = require "auto-dark-mode"
+		config = function()
+			local auto_dark_mode = require("auto-dark-mode")
 
-      auto_dark_mode.setup {
-        update_interval = 30000,
-        set_dark_mode = function()
-          vim.g.nvchad_theme = "monochrome"
-          require("base46").load_all_highlights()
-        end,
-        set_light_mode = function()
-          vim.g.nvchad_theme = "penumbra_light"
-          require("base46").load_all_highlights()
-        end,
-      }
+			auto_dark_mode.setup({
+				update_interval = 30000,
+				set_dark_mode = function()
+					vim.g.nvchad_theme = "monochrome"
+					require("base46").load_all_highlights()
+				end,
+				set_light_mode = function()
+					vim.g.nvchad_theme = "penumbra_light"
+					require("base46").load_all_highlights()
+				end,
+			})
 
-      auto_dark_mode.init()
-    end,
-  },
+			auto_dark_mode.init()
+		end,
+	},
 
-  {
-    "nvimdev/lspsaga.nvim",
-    event = "LspAttach",
-    config = function()
-      require("lspsaga").setup {
-        symbol_in_winbar = {
-          enable = false,
-        },
+	{
+		"nvimdev/lspsaga.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lspsaga").setup({
+				symbol_in_winbar = {
+					enable = false,
+				},
 
-        ui = {
-          theme = "round",
-          title = true,
-          border = "rounded",
-        },
-      }
-    end,
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-  },
+				ui = {
+					theme = "round",
+					title = true,
+					border = "rounded",
+				},
 
-  {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    config = function()
-      require("trouble").setup()
-    end,
-  },
+				lightbulb = {
+					enable = true,
+					enable_in_insert = true,
+					sign = false,
+					sign_priority = 40,
+					virtual_text = true,
+				},
+			})
+		end,
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			--Please make sure you install markdown and markdown_inline parser
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	},
 
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
+	{
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+		config = function()
+			require("trouble").setup()
+		end,
+	},
 
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opt = function()
-      return require "configs.rust-tools"
-    end,
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
-    end,
-  },
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
 
-  {
-    "saecki/crates.nvim",
-    ft = { "rust", "toml" },
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-    end,
-  },
+	{
+		"simrat39/rust-tools.nvim",
+		ft = "rust",
+		dependencies = "neovim/nvim-lspconfig",
+		opt = function()
+			return require("configs.rust-tools")
+		end,
+		config = function(_, opts)
+			require("rust-tools").setup(opts)
+		end,
+	},
 
-  {
-    "shellRaining/hlchunk.nvim",
-    event = "BufReadPost",
-    config = function()
-      require "custom.configs.hlchunk"
-    end,
-  },
+	{
+		"saecki/crates.nvim",
+		ft = { "rust", "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+		end,
+	},
 
-  {
-    "almo7aya/openingh.nvim",
-    cmd = "OpenInGHFile",
-    config = function()
-      require("almo7aya/openingh.nvim").setup()
-    end,
-  },
+	{
+		"shellRaining/hlchunk.nvim",
+		event = "BufReadPost",
+		config = function()
+			require("custom.configs.hlchunk")
+		end,
+	},
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"almo7aya/openingh.nvim",
+		cmd = "OpenInGHFile",
+		config = function()
+			require("almo7aya/openingh.nvim").setup()
+		end,
+	},
 
-  -- {
-  --   "nvim-neotest/neotest",
-  --   ft = "go",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "antoinemadec/FixCursorHold.nvim",
-  --     "nvim-neotest/neotest-go",
-  --   },
-  --   config = function()
-  --     -- get neotest namespace (api call creates or returns namespace)
-  --     local neotest_ns = vim.api.nvim_create_namespace "neotest"
-  --     vim.diagnostic.config({
-  --       virtual_text = {
-  --         format = function(diagnostic)
-  --           local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-  --           return message
-  --         end,
-  --       },
-  --     }, neotest_ns)
-  --     require("neotest").setup {
-  --       -- your neotest config here
-  --       adapters = {
-  --         require "neotest-go",
-  --       },
-  --     }
-  --   end,
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+	-- {
+	--   "nvim-neotest/neotest",
+	--   ft = "go",
+	--   dependencies = {
+	--     "nvim-lua/plenary.nvim",
+	--     "nvim-treesitter/nvim-treesitter",
+	--     "antoinemadec/FixCursorHold.nvim",
+	--     "nvim-neotest/neotest-go",
+	--   },
+	--   config = function()
+	--     -- get neotest namespace (api call creates or returns namespace)
+	--     local neotest_ns = vim.api.nvim_create_namespace "neotest"
+	--     vim.diagnostic.config({
+	--       virtual_text = {
+	--         format = function(diagnostic)
+	--           local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+	--           return message
+	--         end,
+	--       },
+	--     }, neotest_ns)
+	--     require("neotest").setup {
+	--       -- your neotest config here
+	--       adapters = {
+	--         require "neotest-go",
+	--       },
+	--     }
+	--   end,
 }
 
 return plugins
