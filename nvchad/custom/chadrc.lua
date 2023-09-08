9ebc5ed2dae4c5f5e601780afec0deee6fbd2b18
Tom Speak <tom@bere.al>
2023-09-09 00:34:14 +0100

    conf remove gitsigns

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -94,17 +94,6 @@ M.settings = {
 	},
 }
 
-M.gitsigns = {
-	signs = {
-		add = { text = " " },
-		change = { text = " " },
-		delete = { text = " " },
-		topdelete = { text = " " },
-		changedelete = { text = " " },
-		untracked = { text = " " },
-	},
-}
-
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
