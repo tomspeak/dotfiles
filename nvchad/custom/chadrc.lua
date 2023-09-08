1f8942bd9286452eb4b6112da335ce0cc92ecfb4
Tom Speak <tom@bere.al>
2023-09-08 12:47:19 +0100

    things

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "aquarium",
-	theme_toggle = { "aquarium", "aquarium" },
+	theme = "chadracula",
+	theme_toggle = { "chadracula", "chadracula" },
 	transparency = false,
 
 	hl_override = highlights.override,
