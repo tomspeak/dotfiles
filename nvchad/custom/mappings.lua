f3cc324a02d2cdf76cbe56e21effeb3bbf139f4a
Tom Speak <tom@bere.al>
2023-09-10 23:26:35 +0100

    disable c-n to open nerdtree

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,6 +1,12 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+	n = {
+		["<C-n>"] = "",
+	},
+}
+
 M.general = {
 	n = {
 		[";"] = { ":", "enter command mode", opts = { nowait = true } },
