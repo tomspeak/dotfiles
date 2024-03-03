0de5776430abb7ccf0034ee15ba0a7142673859d
Tom Speak <tom@bere.al>
2024-03-03 00:14:33 +0000

    bufferline aesthetics

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -2,18 +2,32 @@ return {
   'akinsho/bufferline.nvim',
   tag = 'v4.5.0',
   event = { 'BufReadPost', 'BufNewFile' },
-  opts = {
-    options = {
-      mode = 'buffers',
-      diagnostics = 'nvim_lsp',
-      always_show_bufferline = true,
-      modified_icon = '*',
-      close_icon = '',
-      show_buffer_close_icons = false,
+  config = function()
+    local bufferline = require 'bufferline'
 
-      offsets = {
-        { filetype = 'NvimTree', text = '', highlight = 'Directory', separator = true },
+    bufferline.setup {
+      options = {
+        mode = 'buffers',
+        style_preset = {
+          bufferline.style_preset.no_italics,
+          bufferline.style_preset.no_bold,
+          bufferline.style_preset.minimal,
+        },
+        separator_style = 'thin',
+        indicator = {
+          style = 'none',
+        },
+        sort_by = 'insert_after_current',
+        themable = true,
+        diagnostics = 'nvim_lsp',
+        always_show_bufferline = true,
+        modified_icon = '*',
+        close_icon = '',
+        show_buffer_close_icons = false,
+        offsets = {
+          { filetype = 'NvimTree', text = '', highlight = 'Directory', separator = true },
+        },
       },
-    },
-  },
+    }
+  end,
 }
