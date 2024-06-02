be5127fc0590677d4b97701200863baa2dfdb780
Tom Speak <tom@bere.al>
2024-06-02 22:59:25 +0100

    init skhd, yabai   & sketchbar

---
+++ sketchybar/plugins/battery.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+CHARGING="$(pmset -g batt | grep 'AC Power')"
+
+if [ "$PERCENTAGE" = "" ]; then
+	exit 0
+fi
+
+ICON=""
+
+if [[ "$CHARGING" != "" ]]; then
+	ICON="[C]"
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set "$NAME" icon="${ICON}" label="${PERCENTAGE}%"
