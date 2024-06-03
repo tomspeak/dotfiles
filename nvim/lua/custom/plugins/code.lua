9569fbe19d036bbafa087e0273ec7c1b78ea4397
Tom Speak <tom@bere.al>
2024-06-03 17:08:39 +0100

    mini.nvim plugins

--- nvim/lua/custom/plugins/code.lua
+++ nvim/lua/custom/plugins/code.lua
@@ -6,23 +6,9 @@ return {
     enabled = vim.fn.has 'nvim-0.10.0' == 1,
   },
   {
-    'kylechui/nvim-surround',
-    version = '*',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {},
-  },
-  {
     'folke/todo-comments.nvim',
     event = { 'BufReadPost', 'BufNewFile' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    opts = {
-      fast_wrap = {},
-      disable_filetype = { 'TelescopePrompt', 'vim' },
-    },
   },
 }
---
+++ nvim/lua/custom/plugins/mini.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'echasnovski/mini.starter',
+    version = '*',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      header = table.concat({
+        '“And when he came to,                                         ',
+        ' he was flat on his back on the beach in the freezing sand,  ',
+        ' and it was raining out of a low sky,                        ',
+        ' and the tide was way out.”                                  ',
+      }, '\n'),
+      items = { name = '', action = ':NvimTreeOpen', section = '' },
+      footer = '',
+    },
+  },
+  {
+    'echasnovski/mini.ai',
+    version = false,
+    opts = {},
+    event = { 'BufReadPost', 'BufNewFile' },
+  },
+  {
+    'echasnovski/mini.align',
+    version = false,
+    opts = {},
+    event = { 'BufReadPost', 'BufNewFile' },
+  },
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    opts = {},
+    event = { 'InsertEnter' },
+  },
+  {
+    'echasnovski/mini.indentscope',
+    version = false,
+    opts = {
+      draw = {
+        delay = 0,
+      },
+    },
+    event = { 'BufReadPost', 'BufNewFile' },
+  },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    opts = {},
+    event = { 'BufReadPost', 'BufNewFile' },
+  },
+}
