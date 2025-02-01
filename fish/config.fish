83b84fcabc0ae21f4011b0b5ea9b6f254feaa1cb
Tom Speak <tom@bere.al>
2025-02-01 00:04:54 +0000

    fish: tweak prompt for block colors/ascii

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if test -f ~/.config/fish/work.fish
   source ~/.config/fish/work.fish
 end
 
-theme_gruvbox dark hard
+theme_gruvbox dark medium
 
 set -x EDITOR 'nvim'
 set -x VEDITOR 'code'
@@ -23,19 +23,34 @@ set __fish_git_prompt_color_cleanstate green
 set __fish_git_prompt_color_dirtystate red
 
 function fish_prompt
-    if test -z "$SSH_CLIENT"
-        set_color -o blue
-    else
-        set_color -o cyan
+    set -l cwd (prompt_pwd)
+
+    set -l cyan (set_color -o cyan)
+    set -l normal (set_color normal)
+
+    set -l time (date '+%I:%M:%S')
+    set -l cwd (basename (prompt_pwd))
+
+    set_color black -b cyan
+    echo -n " $time "
+
+    set_color black -b blue
+    echo -n " $cwd "
+
+    # Show git branch and dirty state
+    set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    if test -n "$git_branch"
+        if test -n "$git_dirty"
+            set_color black -b yellow
+            echo -n " $git_branch "
+        else
+            set_color black -b green
+            echo -n " $git_branch "
+        end
     end
 
-    set_color --bold
-    printf '%s' (pwd)
+    # Add a space
     set_color normal
-
-    fish_git_prompt " %s"
-
-    set_color normal
-
-    echo ' '
+    echo -n ' '
 end
