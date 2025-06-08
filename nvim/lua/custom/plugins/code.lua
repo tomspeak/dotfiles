991da0399c6c60d32dd3f903f436b34e5a933a89
Tom Speak <tom@speak.sh>
2025-06-08 21:36:08 +0100

    nvim: visual-whitespace plugin

--- nvim/lua/custom/plugins/code.lua
+++ nvim/lua/custom/plugins/code.lua
@@ -11,4 +11,16 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
+  {
+    'mcauley-penney/visual-whitespace.nvim',
+    config = true,
+    event = 'ModeChanged *:[vV\22]', -- optionally, lazy load on entering visual mode
+    opts = {
+      fileformat_chars = {
+        unix = '',
+        mac = '',
+        dos = '',
+      },
+    },
+  },
 }
