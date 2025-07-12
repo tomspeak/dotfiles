f0de6ebcc98ed68528d0dc10e281f0faf1661646
Tom Speak <tom@speak.sh>
2025-07-12 15:07:10 +0100

    nvim: delete TODO highlight plugin

--- nvim/lua/custom/plugins/code.lua
+++ nvim/lua/custom/plugins/code.lua
@@ -6,12 +6,6 @@ return {
     enabled = vim.fn.has 'nvim-0.10.0' == 1,
   },
   {
-    'folke/todo-comments.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-  },
-  {
     'mcauley-penney/visual-whitespace.nvim',
     config = true,
     event = 'ModeChanged *:[vV\22]', -- optionally, lazy load on entering visual mode
