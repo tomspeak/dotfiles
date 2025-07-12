31ebe033c0208995690a68a8550c5691cbf791e9
Tom Speak <tom@speak.sh>
2025-07-12 21:48:07 +0100

    nvim: fix debug dap ui

--- nvim/lua/custom/plugins/debug.lua
+++ nvim/lua/custom/plugins/debug.lua
@@ -12,6 +12,13 @@ return {
   },
   keys = {
     {
+      '<leader>db',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
       '<F5>',
       function()
         require('dap').continue()
@@ -63,29 +70,108 @@ return {
     },
   },
   config = function()
-    local dap, dapui = require 'dap', require 'dapui'
+    local dap, dapui, dapvt, dapmason = require 'dap', require 'dapui', require 'nvim-dap-virtual-text', require 'mason-nvim-dap'
 
-    dapui.setup()
+    dapui.setup {
+      controls = {
+        element = 'repl',
+        enabled = true,
+        icons = {
+          disconnect = '',
+          pause = '',
+          play = '',
+          run_last = '',
+          step_back = '',
+          step_into = '',
+          step_out = '',
+          step_over = '',
+          terminate = '',
+        },
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = 'single',
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      force_buffers = true,
+      icons = {
+        collapsed = '',
+        current_frame = '',
+        expanded = '',
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = 'scopes',
+              size = 0.50,
+            },
+            -- {
+            --   id = 'breakpoints',
+            --   size = 0.20,
+            -- },
+            {
+              id = 'stacks',
+              size = 0.25,
+            },
+            {
+              id = 'watches',
+              size = 0.25,
+            },
+          },
+          position = 'right',
+          size = 120,
+        },
+        {
+          elements = {
+            {
+              id = 'repl',
+              size = 0.5,
+            },
+            {
+              id = 'console',
+              size = 0.5,
+            },
+          },
+          position = 'bottom',
+          size = 20,
+        },
+      },
+      mappings = {
+        edit = 'e',
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        repl = 'r',
+        toggle = 't',
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100,
+      },
+    }
+    dapvt.setup {}
+    dapmason.setup {
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-
-    require('mason-nvim-dap').setup {
       automatic_installation = true,
       ensure_installed = { 'codelldb' },
       handlers = {},
     }
-    require('nvim-dap-virtual-text').setup {}
+
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      dapui.open()
+    end
+
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      dapui.close()
+    end
+
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      dapui.close()
+    end
 
     dap.configurations.zig = {
       {
