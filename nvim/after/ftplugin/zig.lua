8e6b29e4a89c389138147590d95b59f7c06247a1
Tom Speak <tom@speak.sh>
2025-07-17 23:24:10 +0100

    nvim: poor mans std.print @l register keybind

--- nvim/after/ftplugin/zig.lua
+++ nvim/after/ftplugin/zig.lua
@@ -3,3 +3,8 @@ vim.opt_local.expandtab = true
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
+
+local esc = vim.api.nvim_replace_termcodes('<Esc>', true, true, true)
+local cr = vim.api.nvim_replace_termcodes('<CR>', true, true, true)
+-- yanks and shoves into print statement when @l is pressed, for a poor mans snippet
+vim.fn.setreg('l', 'yostd.debug.print("{s}\\n", .{' .. esc .. 'pA;' .. esc .. ':w' .. cr)
