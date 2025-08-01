7c5fcee9da78f058f5f0480de7915b88ad7cfeff
Tom Speak <tom@speak.sh>
2025-08-01 23:23:28 +0100

    nvim: nuke conform in favor of lsp

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,20 +3,7 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     notify_on_error = false,
-    formatters_by_ft = {
-      css = { 'prettierd' },
-      html = { 'prettierd' },
-      javascript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      json = { 'prettierd' },
-      lua = { 'stylua' },
-      markdown = { 'prettierd' },
-      rust = { 'rustfmt' },
-      sh = { 'shfmt' },
-      toml = { 'taplo' },
-      typescript = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-    },
+    formatters_by_ft = {},
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
