5eedc9005d1d8408b3a6adff41ec5871bdd5c2c9
Tom Speak <tom@speak.sh>
2025-10-08 22:33:39 +0100

    blink: super-tab keybinds

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -20,88 +20,17 @@ return {
 
   opts = {
     keymap = {
-      preset = 'default',
-      ['<CR>'] = { 'select_and_accept', 'fallback' },
-
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-
-      ['<C-n>'] = { 'select_next', 'snippet_forward', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-
-      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
-
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      preset = 'super-tab',
     },
-    -- appearance = {
-    --   nerd_font_variant = 'normal',
-    -- },
-    -- cmdline = {
-    --   enabled = true,
-    --   keymap = { preset = "cmdline" },
-    --   completion = {
-    --     list = { selection = { preselect = false } },
-    --     menu = {
-    --       auto_show = function()
-    --         return vim.fn.getcmdtype() == ":"
-    --       end,
-    --     },
-    --     ghost_text = { enabled = true },
-    --   },
-    -- },
     completion = {
       trigger = {
         show_on_backspace_after_accept = true,
         show_on_insert = true,
         show_on_trigger_character = true,
+        show_in_snippet = false -- use when using super-tab preset
       },
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
     },
-    --   keyword = {
-    --     range = 'prefix',
-    --   },
-    --   list = {
-    --     selection = { preselect = true, auto_insert = false },
-    --     max_items = 5,
-    --   },
-    -- documentation = {
-    --   window = {
-    --     min_width = 40,
-    --     max_width = 70,
-    --   },
-    --   auto_show = true,
-    --   auto_show_delay_ms = 500,
-    -- },
-    --   menu = {
-    --     min_width = 34,
-    --     max_height = 10,
-    --     draw = {
-    --       treesitter = { "lsp" },
-    --       align_to = 'cursor',
-    --
-    --       padding = 0,
-    --       gap = 0,
-    --       columns = {
-    --         { 'kind_icon',        gap = 1, 'label' },
-    --         { 'label_description' },
-    --       },
-    --       components = {
-    --         kind_icon = {
-    --           text = function(ctx)
-    --             return '[' .. kind_map[ctx.kind] .. ']'
-    --           end,
-    --         },
-    --         label = {
-    --           width = {
-    --             fill = true,
-    --             max = 60,
-    --           },
-    --         },
-    --       },
-    --     },
-    --   },
-    -- },
     snippets = { preset = 'luasnip' },
     sources = {
       default = { 'lsp', 'path', 'snippets' },
