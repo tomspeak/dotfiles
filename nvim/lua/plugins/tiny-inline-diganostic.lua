d384c89ac010b4c554c8bea8feff52bd9e617b6a
Tom Speak <tom@speak.sh>
2025-10-13 23:36:47 +0100

    nvim plugin update: improve diagnostic display options
    - disable lazy loading event for better performance
    - increase severity levels to include HINT for more detailed feedback
    - adjust virtual text settings to enhance readability
    - enable multilines option to handle long messages effectively
    - remove redundant overflow handling configuration
    - simplify and streamline diagnostic options

--- nvim/lua/plugins/tiny-inline-diganostic.lua
+++ nvim/lua/plugins/tiny-inline-diganostic.lua
@@ -1,46 +1,41 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
-  event = "VeryLazy",
+  event = false,
   priority = 1000,
   config = function()
     vim.diagnostic.config({
       update_in_insert = false,
       virtual_text = false,
       severity_sort = true,
-      severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN }
+      severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN, vim.diagnostic.severity.HINT }
     })
 
     require("tiny-inline-diagnostic").setup({
       preset = "classic",
       options = {
+        add_messages = false,
+        multilines = true,
+
         show_source = {
           enabled = false,
-          -- Show source only when multiple sources exist for the same diagnostic
           if_many = true,
         },
         set_arrow_to_diag_color = true,
         throttle = 0,
         softwrap = 30,
 
-
         -- Display all diagnostic messages on the cursor line, not just those under cursor
         show_all_diags_on_cursorline = true,
 
         -- Manage how diagnostic messages handle overflow
         overflow = {
-          -- Overflow handling mode:
-          -- "wrap" - Split long messages into multiple lines
-          -- "none" - Do not truncate messages
-          -- "oneline" - Keep the message on a single line, even if it's long
           mode = "none",
-
-          -- Trigger wrapping this many characters earlier when mode == "wrap"
-          -- Increase if the last few characters of wrapped diagnostics are obscured
           padding = 0,
         },
 
         severity = {
           vim.diagnostic.severity.ERROR,
+          vim.diagnostic.severity.HINT
         },
       },
     })
