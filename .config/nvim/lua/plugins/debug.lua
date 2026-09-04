171bb063c52f61bcf42661f470f7feb2b23e38ed
tom speak <tomspeak@users.noreply.github.com>
2026-09-04 22:34:16 +0100

    [nvim] Keep only customized debugger UI layouts
    Remove copied nvim-dap-ui defaults and the abandoned commented layout. Preserve the existing right-hand scopes, stacks and watches plus bottom REPL and console arrangement.

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -108,35 +108,6 @@ return {
     end
 
     dapui.setup {
-      controls = {
-        element = 'repl',
-        enabled = true,
-        icons = {
-          disconnect = '',
-          pause = '',
-          play = '',
-          run_last = '',
-          step_back = '',
-          step_into = '',
-          step_out = '',
-          step_over = '',
-          terminate = '',
-        },
-      },
-      element_mappings = {},
-      expand_lines = true,
-      floating = {
-        border = 'single',
-        mappings = {
-          close = { 'q', '<Esc>' },
-        },
-      },
-      force_buffers = true,
-      icons = {
-        collapsed = '',
-        current_frame = '',
-        expanded = '',
-      },
       layouts = {
         {
           elements = {
@@ -144,10 +115,6 @@ return {
               id = 'scopes',
               size = 0.50,
             },
-            -- {
-            --   id = 'breakpoints',
-            --   size = 0.20,
-            -- },
             {
               id = 'stacks',
               size = 0.25,
@@ -174,18 +141,6 @@ return {
           position = 'bottom',
           size = 20,
         },
-      },
-      mappings = {
-        edit = 'e',
-        expand = { '<CR>', '<2-LeftMouse>' },
-        open = 'o',
-        remove = 'd',
-        repl = 'r',
-        toggle = 't',
-      },
-      render = {
-        indent = 1,
-        max_value_lines = 100,
       },
     }
     dapvt.setup {}
