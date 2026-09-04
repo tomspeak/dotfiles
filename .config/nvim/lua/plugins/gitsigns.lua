c3a45b4c03319a542e954f87222a86ecaef62b02
tom speak <tomspeak@users.noreply.github.com>
2026-09-04 22:35:06 +0100

    [nvim] Use the supported Gitsigns hunk navigation API
    Replace deprecated next_hunk and prev_hunk wrappers with nav_hunk while preserving counts and native diff-mode motions. Keep existing staging and deleted-line behavior unchanged.

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -42,7 +42,7 @@ return {
           return ']c'
         end
         vim.schedule(function()
-          gs.next_hunk()
+          gs.nav_hunk('next')
         end)
         return '<Ignore>'
       end, { expr = true, desc = 'Jump to next hunk' })
@@ -52,7 +52,7 @@ return {
           return '[c'
         end
         vim.schedule(function()
-          gs.prev_hunk()
+          gs.nav_hunk('prev')
         end)
         return '<Ignore>'
       end, { expr = true, desc = 'Jump to previous hunk' })
