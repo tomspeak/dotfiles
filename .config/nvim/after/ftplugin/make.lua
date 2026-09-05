6dfb542a886c628b87a3fe88136ad6a6a20b36a0
tom speak <tomspeak@users.noreply.github.com>
2026-09-05 15:24:36 +0100

    [nvim] Use native Makefile indentation
    Remove the redundant Make ftplugin override. Native hard-tab settings avoid inserting four tabs per indent in new .mk files while retaining Sleuth inference.

--- .config/nvim/after/ftplugin/make.lua
+++
@@ -1,5 +0,0 @@
-vim.opt_local.shiftwidth = 8
-vim.opt_local.softtabstop = 0
-vim.bo.expandtab = false
-
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin and vim.b.undo_ftplugin .. ' | ' or '') .. 'setlocal shiftwidth< softtabstop< expandtab<'
