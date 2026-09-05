e2e151bb15475cec861d04042fdbc5a1841df93f
tom speak <tomspeak@users.noreply.github.com>
2026-09-05 15:27:56 +0100

    [nvim] Use native motions in the Aerial outline
    Remove j/k actions that ignore counts and duplicate autojump preview. Preserve automatic symbol preview while restoring counted movement such as 3j.

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -2,10 +2,6 @@ return {
   'stevearc/aerial.nvim',
   cmd = { 'AerialOpen', 'AerialToggle' },
   opts = {
-    keymaps = {
-      ['j'] = 'actions.down_and_scroll',
-      ['k'] = 'actions.up_and_scroll',
-    },
     autojump = true,
     show_guides = true,
     attach_mode = 'global',
